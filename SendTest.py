0a253bdc777ad6bf06c6ef7f5deb8f03b6409788
Mina Morgan <minammorgan21@gmail.com.com>
2024-04-22 20:03:45 +0200

    Server Test

---
+++ SendTest.py
@@ -0,0 +1,13 @@
+import requests
+
+url = 'http://13.50.240.154:5000/process'
+image_path = "/home/ubuntu/Distributed-Image-Processing-System-using-Cloud-Computing/image.jpg"  # Replace with the actual path of the image file
+
+files = {'image': open(image_path, 'rb')}
+data = {
+    'text': 'HELLLOOOO',
+    'operation': 'edge_detection'  # Specify the desired operation (edge_detection or color_inversion)
+}
+
+response = requests.post(url, files=files, data=data)
+print(response.json())
