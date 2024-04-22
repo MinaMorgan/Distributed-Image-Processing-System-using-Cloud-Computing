43d890bbecd49fff2dca28fe0eb3b86c0c65ac70
Mina Morgan <minammorgan21@gmail.com.com>
2024-04-22 20:20:08 +0200

    Phase 2 intial Commit

--- Send.py
+++ Send.py
@@ -1,7 +1,7 @@
 import requests
 
 url = 'http://13.50.240.154:5000/process'
-image_path = "/home/ubuntu/Distributed-Image-Processing-System-using-Cloud-Computing/image.jpg"  # Replace with the actual path of the image file
+image_path = "C:\\Users\\DELL\\Desktop\\Spring 24\\Distributed Computing\\Project\\Github\\Distributed-Image-Processing-System-using-Cloud-Computing\\image.jpg"  # Replace with the actual path of the image file
 
 files = {'image': open(image_path, 'rb')}
 data = {
